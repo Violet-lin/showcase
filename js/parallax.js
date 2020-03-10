8ad38274dc37985cb16f47800b0c3da87caf0f5b
Violet Lin <p0449201@gmail.com>
2020-03-10 08:26:16 +0800

    update: new scroll effect

--- js/parallax.js
+++ js/parallax.js
@@ -3,18 +3,40 @@ $(document).ready(function(){
 
   var controller = new ScrollMagic.Controller();
 
-  var homeScrollScene = new  ScrollMagic.Scene({
-    triggerElement: '.nav-wrapper',
-    triggerHook:0.5
+  var homeScrollScene = new ScrollMagic.Scene({
+    triggerElement: '.pDesign',
+    triggerHook:0.5,
+    duration: 916
   })
-  .setClassToggle('.nav-wrapper','fade-in')
-  // .addIndicators()
+
+  .setPin("#pDesign")
+
+  // .setClassToggle('.pDesign','fade-in')
+  // .addIndicators({name: "1 (duration: 900)"})
   .addTo(controller);
 
+  var homeScrollScene2 = new ScrollMagic.Scene({
+    triggerElement: '.pDev',
+    triggerHook:0.5,
+    duration: 300
+  })
+  .setPin("#pDev")
+  // .addIndicators({name: "2 (duration: 300)"})
+  .addTo(controller);
+
+  new ScrollMagic.Scene({
+							triggerElement: "#trigger1",
+							triggerHook: 0.9, // show, when scrolled 10% into view
+							duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
+							offset: 50 // move trigger to center of element
+						})
+						.setClassToggle("#reveal1", "visible") // add class to reveal
+						// .addIndicators() // add indicators (requires plugin)
+						.addTo(controller);
   // ==== Parallax animation ====
 
 });
-$('.parallax2').parallax();
+$('.parallax_area').parallax();
 $('.swipebox').swipebox();
 $('h1').css({
   'width': '100%',
